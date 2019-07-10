47f65a86e644a2066ce239081803c826a54ae5c2
Anmol <anmol.agarwal2004@yahoo.com>
2019-07-10 23:02:52 +0530

    MentorList Adapter Issue Remaining

---
+++ app/src/main/java/com/sibmentors/appointment/MentorListAdapter.kt
@@ -0,0 +1,36 @@
+package com.sibmentors.appointment
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.TextView
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.FirebaseDatabase
+
+class MentorListAdapter(val mCtx: Context, val layoutId: Int, val mentorList: List<String>) :
+    ArrayAdapter<String>(mCtx, layoutId, mentorList) {
+    val currentUser = FirebaseAuth.getInstance().currentUser
+    val ref = FirebaseDatabase.getInstance().getReference("Slots")
+    val userref = FirebaseDatabase.getInstance().getReference("users")
+
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
+
+        val layoutInflater: LayoutInflater = LayoutInflater.from(mCtx)
+        val view: View = layoutInflater.inflate(layoutId, null)
+        val mentorname = view.findViewById<TextView>(R.id.mentorName)
+        val mentorid = view.findViewById<TextView>(R.id.mentorId)
+        var mentorlistvalue= mentorList
+        val listmentor = mentorList[position]
+
+
+
+
+
+        mentorname.text = listmentor[0].toString()
+        mentorid.text = "id"
+        return view
+    }
+}
