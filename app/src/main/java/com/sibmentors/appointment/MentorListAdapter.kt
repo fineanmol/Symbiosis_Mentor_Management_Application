894f7dfdba9945f8fc421d80701be67b8f6aa772
Anmol <anmol.agarwal2004@yahoo.com>
2019-07-11 23:16:14 +0530

    id fetched delete part added without student sync

--- app/src/main/java/com/sibmentors/appointment/MentorListAdapter.kt
+++ app/src/main/java/com/sibmentors/appointment/MentorListAdapter.kt
@@ -1,14 +1,23 @@
 package com.sibmentors.appointment
 
+
 import android.content.Context
+import android.content.DialogInterface
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
 import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import androidx.core.text.isDigitsOnly
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
+import java.util.regex.Pattern
 
 class MentorListAdapter(val mCtx: Context, val layoutId: Int, val mentorList: List<String>) :
     ArrayAdapter<String>(mCtx, layoutId, mentorList) {
@@ -23,14 +32,119 @@ class MentorListAdapter(val mCtx: Context, val layoutId: Int, val mentorList: Li
         val view: View = layoutInflater.inflate(layoutId, null)
         val mentorname = view.findViewById<TextView>(R.id.mentorName)
         val mentorid = view.findViewById<TextView>(R.id.mentorId)
+
         val listmentor = mentorList[position]
-        Log.d("TAG1",listmentor)
+        Log.d("TAG1", listmentor)
+        mentorname.text = listmentor
+        var extractedid=""
+        for(i in listmentor){
+                if(i.isDigit())
+                extractedid+=i
+        }
+
+        mentorid.text = extractedid
+
+        currentUser?.let { user ->
+
+            val userNameRef = userref.parent?.child("users")?.orderByChild("email")?.equalTo(user.email)
+            val eventListener = object : ValueEventListener {
+                override fun onDataChange(dataSnapshot: DataSnapshot) = if (!dataSnapshot.exists()) {
+                    //create new user
+                    Toast.makeText(mCtx, "User details not found", Toast.LENGTH_LONG).show()
+                    //logout()
+                } else {
+
+                    for (e in dataSnapshot.children) {
+                        val employee = e.getValue(Data::class.java)
+                        var refercode = employee!!.mentorreferal
+                        deletebtnclickmethod(view, refercode, listmentor)
+
+
+                    }
+                }
+
+                override fun onCancelled(databaseError: DatabaseError) {
+                }
+            }
+            userNameRef?.addListenerForSingleValueEvent(eventListener)
+
+        }
 
 
 
-
-        mentorname.text = listmentor.toString()
-        mentorid.text = "id"
         return view
     }
-}
+
+    private fun deletebtnclickmethod(
+        view: View,
+        refercode: String,
+        listmentor: String
+    ) {
+        val deletebtn = view.findViewById<TextView>(R.id.delete)
+        deletebtn.setOnClickListener {
+            val alertbox = AlertDialog.Builder(mCtx)
+                .setMessage("Do you want to Delete this Mentor !!\nYou have to add this mentor's code again if you want to see their slots")
+                .setPositiveButton("Delete", DialogInterface.OnClickListener { arg0, arg1 ->
+                    // do something when the button is clicked
+                    //region StudentBookButtonFunction
+
+
+              /*      currentUser?.let { user ->
+                        // Toast.makeText(mCtx, user.email, Toast.LENGTH_LONG).show()
+                        val userNameRef = ref.parent?.child("users")?.orderByChild("email")?.equalTo(user.email)
+                        val eventListener = object : ValueEventListener {
+                            @SuppressLint("ResourceAsColor")
+                            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                                if (!dataSnapshot.exists()) {
+                                    //create new user
+                                    Toast.makeText(mCtx, "No Appointments are Available Yet!!", Toast.LENGTH_LONG)
+                                        .show()
+                                } else {
+                                    for (e in dataSnapshot.children) {
+                                        val employee = e.getValue(Data::class.java)
+                                        var studentId = employee?.studentId
+                                        var studentName = employee?.name
+                                        var phone = employee?.number
+                                        var studentkey = employee?.id
+                                        var status = employee?.status
+                                        var mentorrefercodes = employee!!.mentorreferal
+
+                                        var mentorcodes = mentorrefercodes.split("/")
+                                        for (i in mentorcodes) {
+                                            if (i.split(":").last() == "NB" ) {
+
+                                                var newrefercodes=mentorrefercodes.replace(i,"$:B")
+                                                userref.child(studentkey!!).child("mentorreferal").setValue(newrefercodes)
+
+                                                Toast.makeText(
+                                                    mCtx,
+                                                    "$studentName Appointment Booked! \n at: ",
+                                                    Toast.LENGTH_LONG
+                                                ).show()
+
+                                            }
+                                            if (i.split(":").last() == "B" ) {
+
+                                                Toast.makeText(mCtx, "You have already booked an appointment.", Toast.LENGTH_LONG).show()
+                                            }
+                                        }
+
+                                    }
+                                }
+                            }
+
+                            override fun onCancelled(databaseError: DatabaseError) {
+                            }
+                        }
+                        userNameRef?.addListenerForSingleValueEvent(eventListener)
+
+                    }*/
+                })
+                .setNegativeButton("No", // do something when the button is clicked
+                    DialogInterface.OnClickListener { arg0, arg1 -> })
+                .show()
+
+
+                }
+        }
+    }
